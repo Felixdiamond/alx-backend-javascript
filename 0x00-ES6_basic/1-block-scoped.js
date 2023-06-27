b8d5207c245eed3b56eed7231f745f4b1e58738f
Felixdiamond <diamondfelix006@gmail.com>
2023-06-27 13:44:47 -0700

    0x00-ES6_basic

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,11 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
 }
+
