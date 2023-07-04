bb1594a72412aafc0b2dea8538e2171dfd0068cb
Felixdiamond <diamondfelix006@gmail.com>
2023-07-04 14:14:51 -0700

    ### ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+const getListStudentIds = (arrayOfObjects) => {
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+  return arrayOfObjects.map((student) => student.id);
+};
+
+export default getListStudentIds;
