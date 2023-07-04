f2a784642a5b9e50f124de04b038f29999ae485c
Felixdiamond <diamondfelix006@gmail.com>
2023-07-04 15:02:42 -0700

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+};
+
+export default updateStudentGradeByCity;
+
