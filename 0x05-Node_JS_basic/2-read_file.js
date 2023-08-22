139427094b09b4ed1a41ef59c808016cb7e97a9b
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-08-22 22:40:58 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,13 +7,13 @@ function countStudents(path) {
     const students = lines.slice(1).map((line) => line.split(',')).filter((student) => student.length === 4);
     console.log(`Number of students: ${students.length}`);
     const fields = {};
-    for (const student of students) {
+    students.forEach((student) => {
       if (!fields[student[3]]) fields[student[3]] = [];
       fields[student[3]].push(student[0]);
-    }
-    for (const field in fields) {
+    });
+    Object.keys(fields).forEach((field) => {
       console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-    }
+    });
   } catch (err) {
     throw new Error('Cannot load the database');
   }
