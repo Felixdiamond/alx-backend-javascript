bf0cf03a6035ad283f27c9e2b4ba37dd2297dbcd
Felixdiamond <diamondfelix006@gmail.com>
2023-08-22 20:00:52 +0100

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n');
+    let students = lines.slice(1).map(line => line.split(',')).filter(student => student.length === 4);
+    console.log(`Number of students: ${students.length}`);
+    const fields = {};
+    for (const student of students) {
+      if (!fields[student[3]]) fields[student[3]] = [];
+      fields[student[3]].push(student[0]);
+    }
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
+
