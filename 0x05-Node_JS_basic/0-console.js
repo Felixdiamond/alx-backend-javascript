b31c0063890c9af1a265c0a503b333aa2acf0e56
Felixdiamond <diamondfelix006@gmail.com>
2023-08-22 19:50:37 +0100

    NodeJS Basics

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,10 @@
-function displayMessage(message) {
+/**
+ * displayMessage - prints a message to the standard output
+ * @message: the message to print
+ */
+const displayMessage = (message) => {
   console.log(message);
-}
+};
 
 module.exports = displayMessage;
 
