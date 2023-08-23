a5a3754185efb0dbdaca0f171e068653c0849f59
Felixdiamond <diamondfelix006@gmail.com>
2023-08-23 19:07:30 +0000

    JS unittests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
   it('should return the sum of two rounded numbers', function() {
@@ -8,4 +8,13 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  
+  // Add more test cases here
+  it('should handle negative numbers', function() {
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -6);
+  });
 });
+
